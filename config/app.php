611fd66e32b35ab9a51b3dda4364b4bf8bbd9968
unknown <facubal1991@hotmail.com>
2019-08-25 19:15:02 -0300

    TESTING

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', "base64:k6c8t5NahmXcMV38EOfcbJVydevjr5BirR8JY4d4Jz0="),
 
     'cipher' => 'AES-256-CBC',
 
